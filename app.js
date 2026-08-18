110884665c675e075077e9e02a5b71a7df238604
kapil soni <kapilsoni2404@gmail.com>
2026-08-19 00:29:34 +0530

    Fix error view

--- app.js
+++ app.js
@@ -137,7 +137,7 @@ app.use((req,res,next)=>{
 app.use((err,req,res,next)=>{
     // console.log(err);
     let{status=500,message="something went wrong"}=err;
-    res.status(status).render("Error.ejs",{message});
+    res.status(status).render("error.ejs",{message});
     // res.status(status).send(message);
 });
 app.listen(8080,()=>{
